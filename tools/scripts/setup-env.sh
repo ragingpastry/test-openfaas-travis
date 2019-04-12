8795c3a4a2d0ebcac31cf5a66c5092eb2740f559
Nick Wilburn <senior.crepe@gmail.com>
2019-04-12 08:17:40 -0500

    test

---
+++ tools/scripts/setup-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ ! $(which docker) ]]; then
+  echo "Docker must be installed. Exiting"
+fi
+
+openfaas_repository="https://github.com/openfaas/faas"
+
+function_repositories=(
+  pipedpiper-flake8-faas
+  piedpiper-validator-faas
+  piedpiper-cpplint-faas
+)
+
+docker swarm init
+mkdir -p faas
+pushd faas
+git clone "${openfaas_repository}" .
+./deploy_stack.sh --no-auth
+curl -sL https://cli.openfaas.com | sudo sh
+popd
+
+for repository in function_repositories; do
+  git clone https://github.com/afcyber-dream/"${repository}"
+  pushd "${repository}"
+  faas build && faas deploy
+  popd
+done;
+
